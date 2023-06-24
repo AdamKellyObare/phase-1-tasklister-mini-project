babc5bb382de3892dd7b27bd4c6fe0b9aa742ce1
adamKellyObare <aobarekelly@gmail.com>
2023-06-24 10:36:47 +0300

    Assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.querySelector('form')
+
+  newTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const newToDo = e.target.newtaskdescription.value
+    buildToDo(newToDo);
+
+    newTaskForm.reset()
+  })
 });
+
+function buildToDo(newToDo){
+
+  let taskLi = document.creaateElement('Li');
+  let deleteButton =  document.createElement('button')
+
+  deleteButton.textContent = `${newToDo}`
+
+  taskLi.appendChild(deleteButton);
+
+  document.querySelector('#tasks').appendChild(taskLi);
+
+  deleteButton.addEventListener('click', handleDelete)
+}
+
+function handleDelete(e){
+
+  e.target.parentNode.remove();
+  
+}
